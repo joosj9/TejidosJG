38239b2a58b73bb03e4badc7c723572169f4d84a
joosj9 <joselingcelestegonzalez@gmail.com>
2025-10-14 19:38:25 -0600

    Add files via upload

---
+++ PAGINA WEB EMPRENDIMIENTO/main.js
@@ -0,0 +1,5 @@
+let nombre = prompt("Ingrese su nombre, por favor:");
+let apellido = prompt("Ingrese su apellido, por favor:");
+let nombreCompleto = nombre + " " + apellido;
+alert("Bienvenido/a " + nombreCompleto + " a nuestra página de emprendimiento!");
+
